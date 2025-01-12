99eed14fff818df07b12a5324264873d57f6ef70
notZairus <notzairus@gmail.com>
2025-01-12 16:27:42 +0700

    user camera

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-
+import { useState, useEffect, useRef } from "react"
 
 export default function App() {
+  
+  const [tryy, setTryy] = useState(1);
+  const videoRef = useRef(null);
+
+  useEffect(async () => {
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      videoRef.current.srcObject = stream;
+    } catch(error) {
+      console.error(error);
+    }
+
+  }, [])
+
+
   return (
     <>
       <div className="w-screen h-screen bg-gray-500 flex items-center justify-center">
         <main className="min-h-20 w-[360px] bg-zinc-800 rounded text-white py-4 px-2 space-y-4">
+          
           <section className="px-2">
             <h1 className="text-4xl font-bold">RG</h1>
           </section>
+
           <section>
-            <video className="w-full h-96 bg-black rounded-lg">
+            <video ref={videoRef} className="w-full h-96 bg-black rounded-lg">
 
             </video>
           </section>
+
         </main>
       </div>
     </>
