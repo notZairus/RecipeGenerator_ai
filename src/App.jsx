1f133f6ec87d8d95adb143872f25a692d38d3bab
notZairus <notzairus@gmail.com>
2025-01-12 17:32:18 +0700

    full screen on smaller devices

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
   return (
     <>
       <div className="w-screen h-screen bg-gray-500 flex items-center justify-center">
-        <main className="min-h-20 w-[360px] bg-zinc-800 rounded text-white py-4 px-2 space-y-4">
+        <main className="sm:w-full sm:h-full lg:w-[360px] lg:h-auto h-auto bg-zinc-800 rounded text-white py-4 px-2 space-y-4">
           
           <section className="px-2">
             <h1 className="text-4xl font-bold">RG</h1>
