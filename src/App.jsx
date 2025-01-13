9263b3d5c2cca01537bf678cbdabe2ac34dda444
notZairus <notzairus@gmail.com>
2025-01-13 18:40:29 +0700

    added my name

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react"
+import cameraIcon from "./assets/camera.svg";
 
 export default function App() {
   
@@ -19,17 +20,34 @@ export default function App() {
     startCamera();
   }, [])
 
+  function capturePicture(e) {
+    alert('you took a pic');
+
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+
+    context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+
+    const image = canvas.toBlob((blob) => {
+
+    })
+  }
+
 
   return (
     <>
       <div className="w-screen min-h-screen bg-zinc-800 flex items-center justify-center">
         <main className="w-full h-full rounded text-white py-4 px-2 space-y-4">
           
-          <section className="px-2">
+          <section className="px-2 justify-between items-center">
             <h1 className="text-4xl font-bold">RG</h1>
+            <p className="text-white/40 mt-1">Developer: Zairus V. Bermillo</p>
           </section>
 
-          <section className="bg-yellow-400 relative">
+          <section className="relative">
             <video 
               ref={videoRef} 
               className="w-full h-96 bg-black rounded-lg"
@@ -37,10 +55,9 @@ export default function App() {
               playsInline
             >
             </video>
-            <button className="aspect-square w-20 bg-zinc-800 absolute m-auto flex items-center justify-center rounded-full inset-x-0 bottom-8">
-                Capture
+            <button onClick={capturePicture} className="aspect-square w-20 bg-zinc-800 absolute m-auto flex items-center justify-center rounded-full inset-x-0 bottom-8">
+              <img src={cameraIcon} alt="camera-icon" className="w-2/3"/>
             </button>
-            
           </section>
 
           <section className="w-full h-auto min-h-20 flex justify-center items-center">
